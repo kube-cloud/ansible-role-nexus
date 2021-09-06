bcd7ea01bafe05cda296f0ff49db8751cd8675c7
ETUNE NGI Jean-Jacques <jetune@leadware.net>
2021-09-06 18:45:47 +0200

    Updates

--- scripts/groovy/configure_admin_password.groovy
+++
@@ -1,7 +0,0 @@
-import groovy.json.JsonSlurper
-
-// Transform submitted argument to JSon Structure
-jsonArguments = new JsonSlurper().parseText(args)
-
-// Initialize Admin Password
-security.securitySystem.changePassword(jsonArguments.adminUserName, jsonArguments.newAdminPassword)
